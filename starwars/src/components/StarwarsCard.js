2a6bce10d5c56a97df3e62703d15b37ca145fe2a
Carlos Baez <carlosbaez@Green-Planet.local>
2019-11-24 22:31:39 -0500

    fixxed the props.hairColor & props.skinColor

--- starwars/src/components/StarwarsCard.js
+++ starwars/src/components/StarwarsCard.js
@@ -9,8 +9,8 @@ const StarwarsCard = props => {
                     <h4><CardTitle>{props.name}</CardTitle></h4>
                     <CardText className="text-muted">Birth: {props.birthYear}</CardText>
                     <CardText className="text-muted">Gender: {props.gender}</CardText>
-                    <CardText className="text-muted">Skin Color: {props.skin_color}</CardText>
-                    <CardText className="text-muted">Hair Color: {props.hair_color}</CardText>
+                    <CardText className="text-muted">Skin Color: {props.skinColor}</CardText>
+                    <CardText className="text-muted">Hair Color: {props.hairColor}</CardText>
                     <CardText className="text-muted">Height: {props.height}</CardText>
                 </CardBody>
             </Card>
