a9691bccad4bc41262b8eef589389791b8f96cf5
Carlos Baez <carlosbaez@Green-Planet.local>
2019-11-24 22:23:51 -0500

    Pulled the data and created the cards

---
+++ starwars/src/components/StarwarsCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card, CardBody, CardTitle, CardText, Col } from "reactstrap";
+
+const StarwarsCard = props => {
+    return (
+        <Col sm="6" lg="4" key={props.name}>
+            <Card style={{ background: "white", borderColor: "yellow" }}>
+                <CardBody>
+                    <h4><CardTitle>{props.name}</CardTitle></h4>
+                    <CardText className="text-muted">Birth: {props.birthYear}</CardText>
+                    <CardText className="text-muted">Gender: {props.gender}</CardText>
+                    <CardText className="text-muted">Skin Color: {props.skin_color}</CardText>
+                    <CardText className="text-muted">Hair Color: {props.hair_color}</CardText>
+                    <CardText className="text-muted">Height: {props.height}</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    )
+}
+
+export default StarwarsCard;
